7bed224ffde0aa31d2fdb29a3386dade43b25548
at-rooshee <ru_sha_nia@yahoo.ca>
2016-09-20 00:53:48 -0500

    Adding input form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,8 +7,25 @@ import './body.html';
   tasks() {
 
     return Tasks.find({});
+   },
+});
 
-  },
+Template.body.events({
+ 'submit .new-task'(event) {
+  // Prevent default browser form submit
+  event.preventDefault();
+  // Get value from form element
+   const target = event.target;
+   const text = target.text.value;
+  // Insert a task into the collection
+  Tasks.insert({
+     text,
+     createdAt: new Date(), // current time
+   });
+  // Clear form
+  target.text.value = '';
+
+   },
 
 });
 
