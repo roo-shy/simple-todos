bcd66e2e967d1b09742493004c680bf560cf58b4
at-rooshee <ru_sha_nia@yahoo.ca>
2017-03-06 00:07:58 -0600

    Checking collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,15 +5,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Task from './Task.jsx';
 
 //App component -represents the whole App
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'Task 1' },
-      { _id: 2, text: 'Task 2' },
-      { _id: 3, text: 'Task 3' },
-    ];
-  }
-  
+class App extends Component {
   renderTasks() {
     return this.getTasks().map((task) => (
       <Task key={task._id} task={task} />
@@ -33,3 +25,4 @@ export default class App extends Component {
     );
   }
 }
+
--- imports/ui/tasks.js
+++ imports/ui/tasks.js
@@ -1,2 +1,3 @@
 import { Mongo } from 'meteor/mongo';
+
 export const Tasks = new Mongo.Collection('tasks');
